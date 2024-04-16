fa63b97473da140d76829a18b0ab72cc084b650c
Hisham Mohammed <hishammohammed1199@gmail.com>
2024-04-16 22:34:38 +0300

    handle unchecked exceptions on EmployeeController class

--- src/com/hishamfactory/EmployeesController.java
+++ src/com/hishamfactory/EmployeesController.java
@@ -2,11 +2,12 @@ package com.hishamfactory;
 
 import com.sun.security.jgss.GSSUtil;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class EmployeesController {
     Scanner sc = new Scanner(System.in);
-    public  Employee getEmployeeByName(String first_last_name){
+    public Employee getEmployeeByName(String first_last_name){
         int i = 0;
         while(i < Company.employees.size()){
             String name = Company.employees.get(i).getFirst_name()+ " " + Company.employees.get(i).getLast_name();
@@ -36,10 +37,10 @@ public class EmployeesController {
         }
     }
     public  void editEmployeeInfo(String name){
-        Employee passenger = getEmployeeByName(name);
-        String passenger_name = passenger.getFirst_name() +" " + passenger.getLast_name();
         try {
-            if(passenger_name.equals(name)){
+        Employee employee = getEmployeeByName(name);
+        String employee_name = employee.getFirst_name() +" " + employee.getLast_name();
+            if(employee_name.equals(name)){
                 System.out.println("1.Edit Name");
                 System.out.println("2.Edit Age");
                 System.out.println("3.Edit Tel Number");
@@ -50,26 +51,30 @@ public class EmployeesController {
                 switch (option){
                     case  1:
                         System.out.print("Enter new first name: ");
-                        passenger.setFirst_name(sc.next());
+                        employee.setFirst_name(sc.next());
                         System.out.print("Enter new last name: ");
-                        passenger.setLast_name(sc.next());
+                        employee.setLast_name(sc.next());
                         System.out.println("Name Changed");
                         break;
                     case 2:
                         System.out.print("Enter new age: ");
-                        passenger.setAge(sc.nextInt());
+                        employee.setAge(sc.nextInt());
                         System.out.println("Age changed");
                         break;
 
                     case 3:
-                        System.out.print("Enter new telefon number: ");
-                        passenger.setTel_number(sc.next());
-                        System.out.println("Telefon number changed");
+                        System.out.print("Enter new tel number: ");
+                        employee.setTel_number(sc.next());
+                        System.out.println("Tel number changed");
                         break;
                 }
             }
-        }catch (Exception e){
-            System.out.println("The given value is null.check it");
+        }catch (NoSuchElementException e){
+            System.out.println("Input not found. Please enter text without spaces");
+            sc.next();
+        }catch (NullPointerException e){
+            System.out.println("A NullPointerException occurred.System will exit");
+            System.exit(0);
         }
     }
     public void deleteEmployee(String name){
@@ -77,57 +82,62 @@ public class EmployeesController {
     }
     public void fireEmployee(String name){
         //Update Employee Status
-        Employee employee = getEmployeeByName(name);
-        employee.setStatus("non-active");
-        System.out.println("Status: " + employee.getStatus());
-        //Notify Relevant Parties
-        System.out.println("Send to relevant parties...done");
-        //Revoke Access Privileges
-        System.out.println("Revoke Access Privileges...done");
-    }
-    public void showEmployeeMenu(Company company,EmployeesController controller){
-        System.out.println(".......................Employee Menu..........................");
-        System.out.println("1.Add new employee");
-        System.out.println("2.Get employee by name");
-        System.out.println("3.Show all employees");
-        System.out.println("4.Edit employee info");
-        System.out.println("5.Delete employee");
-        System.out.println("6.Fire employee");
-        System.out.print("Enter a choice: ");
-        int option = sc.nextInt();
-        String name = "";
-        if(option != 1  && option != 3){
-            System.out.print("Enter employee first name: ");
-            name = sc.next();
-            System.out.print("Enter employee last name: ");
-            name += " " + sc.next();
+        try {
+            Employee employee = getEmployeeByName(name);
+            employee.setStatus("non-active");
+            System.out.println("Status: " + employee.getStatus());
+            //Notify Relevant Parties
+            System.out.println("Send to relevant parties...done");
+            //Revoke Access Privileges
+            System.out.println("Revoke Access Privileges...done");
+        }catch (NullPointerException e){
+            System.out.println("A NullPointerException occurred.System will exit");
+            System.exit(0);
         }
-        switch (option) {
-            case 1:
-                company.addEmployee(company);
-                break;
-            case 2:
-                try {
+    }
+    public void showEmployeeMenu(Company company,EmployeesController controller) {
+        try {
+            System.out.println(".......................Employee Menu..........................");
+            System.out.println("1.Add new employee");
+            System.out.println("2.Get employee by name");
+            System.out.println("3.Show all employees");
+            System.out.println("4.Edit employee info");
+            System.out.println("5.Delete employee");
+            System.out.println("6.Fire employee");
+            System.out.print("Enter a choice: ");
+            int option = sc.nextInt();
+            String name = "";
+            if (option != 1 && option != 3) {
+                System.out.print("Enter employee first name: ");
+                name = sc.next();
+                System.out.print("Enter employee last name: ");
+                name += " " + sc.next();
+            }
+            switch (option) {
+                case 1:
+                    company.addEmployee(company);
+                    break;
+                case 2:
                     Employee employee = controller.getEmployeeByName(name);
                     System.out.println(employee.toString());
-
-                } catch (NullPointerException e) {
-                    System.out.println("returned null");
-                }
-                break;
-            case 3:
-                System.out.println("show all employees: ");
-                controller.printAllEmployees();
-                break;
-            case 4:
-                controller.editEmployeeInfo(name);
-                break;
-            case 5:
-                controller.deleteEmployee(name);
-                break;
-            case 6:
-                controller.fireEmployee(name);
-                break;
+                    break;
+                case 3:
+                    System.out.println("show all employees: ");
+                    controller.printAllEmployees();
+                    break;
+                case 4:
+                    controller.editEmployeeInfo(name);
+                    break;
+                case 5:
+                    controller.deleteEmployee(name);
+                    break;
+                case 6:
+                    controller.fireEmployee(name);
+                    break;
+            }
+        }catch (NoSuchElementException e){
+            System.out.println("Input not found. Please enter text without spaces");
+            sc.next();
         }
     }
 }
