0557c658fe249b31678492f51d822092f982673a
Hisham Mohammed <hishammohammed1199@gmail.com>
2024-12-10 19:20:07 +0300

    add testing code for AirportController class

--- src/test/java/com/hishamfactory/AirportControllerTest.java
+++ src/test/java/com/hishamfactory/AirportControllerTest.java
@@ -1,6 +1,45 @@
 package com.hishamfactory;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AirportControllerTest {
 
+    Airport istanbulAirport;
+    Airport adenAirport;
+    AirportController airportsController;
+
+    @BeforeEach
+    public void setup(){
+        istanbulAirport = new Airport("Istanbul","Turkey",24,100,new Company("Turkish Government"));
+        adenAirport = new Airport("Aden","Yemen",44,50,new Company("Yemeni Government"));
+        Company.airports.add(istanbulAirport);
+        Company.airports.add(adenAirport);
+        airportsController = new AirportController();
+    }
+
+    @Test
+    public void returnedAirportByNameSame(){
+        assertSame(istanbulAirport, AirportController.getAirportByName("Istanbul"));
+    }
+    @Test
+    public void returnedAirportByNameNotSame(){
+        assertNotSame(adenAirport, AirportController.getAirportByName("Istanbul"));
+    }
+    @Test
+    public void returnedAirportByUuidSame(){
+        assertSame(istanbulAirport,AirportController.getAirportByID(istanbulAirport.getAirport_uuid()));
+    }
+    @Test
+    public void returnedAirportByUuidNotSame(){
+        assertNotSame(adenAirport,AirportController.getAirportByName(istanbulAirport.getAirport_uuid()));
+    }
+    @Test
+    public void deletedAirportIsTrue(){
+        airportsController.deleteAirport(istanbulAirport.getAirport_uuid());
+        assertFalse(Company.airports.contains(istanbulAirport));
+    }
 
 }
